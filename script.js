51d1f8a4dfbde4ce5b322fcb7369a2dfbc20a599
Armen998 <91012248+Armen998@users.noreply.github.com>
2021-09-22 19:02:17 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("Tnayin 1.1");
-for(let i=1; i<=100; i++){
+   for(let i=1; i<=100; i++){
     console.log("i =", i)
   }
  
@@ -9,24 +9,24 @@ console.log("Tnayin 1.2");
   }
  
 console.log("Tnayin 2");
-  for(let i=2; i<100; i++ ){
+   for(let i=2; i<100; i++ ){
     console.log("i = ", i=i+i%2)
   }
 
-  console.log("Tnayin 3");
+console.log("Tnayin 3");
   let sum = 0
   for(let i=1; i<=100; i++ ){
     sum=sum+i
   }
-  console.log( "1-ic 100 tveri gumar@ =", sum)
+console.log( "1-ic 100 tveri gumar@ =", sum)
   
-  console.log("Tnayin 4");
+console.log("Tnayin 4");
   let arr = [1, 2, 3, 4, 5]
   for(let i=0; i<arr.length; i++){
-      console.log("i =", arr[i])
-    }
+   console.log("i =", arr[i])
+  }
 
-  console.log("Tnayin 5");
+console.log("Tnayin 5");
   let newArr = [2, 5, 9, 15, 0, 4]
   for(let i=0; i<newArr.length; i++){
     if (newArr[i]>3 && newArr[i]<10){
