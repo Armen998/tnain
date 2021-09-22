2ac5a41667b80904f5bf567e063624271a69e664
Armen998 <gevorgyan.armen98@inbox.ru>
2021-09-22 14:18:08 +0400

    tnayin

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-for(let i=1; i<=5; i++){
-    alert(i)
-   }
-   
+console.log("Tnayin 1.1");
+for(let i=1; i<=100; i++){
+    console.log("i =", i)
+  }
+ 
+console.log("Tnayin 1.2");
+   for(let i=11; i<=33; i++){
+    console.log("i =", i)
+  }
+ 
+console.log("Tnayin 2");
+  for(let i=2; i<100; i++ ){
+    console.log("i = ", i=i+i%2)
+  }
+
+  console.log("Tnayin 3");
+  let sum = 0
+  for(let i=1; i<=100; i++ ){
+    sum=sum+i
+  }
+  console.log( "1-ic 100 tveri gumar@ =", sum)
+  
+  console.log("Tnayin 4");
+  let arr = [1, 2, 3, 4, 5]
+  for(let i=0; i<arr.length; i++){
+      console.log("i =", arr[i])
+    }
+
+  console.log("Tnayin 5");
+  let newArr = [2, 5, 9, 15, 0, 4]
+  for(let i=0; i<newArr.length; i++){
+    if (newArr[i]>3 && newArr[i]<10){
+      console.log("i =", newArr[i])
+    }
+  }
